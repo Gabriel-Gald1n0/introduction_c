bd197b783bf302e4ecb858e1a7327754e55b8450
gabriel <galdinobrossx1@gmail.com>
2023-08-22 16:51:05 -0300

    variaveis.c create

---
+++ Aula 01/variaveis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h> 
+
+int main(){
+    int a;
+    a = 2;
+
+    int b = 4;
+    /*
+    outras maneiras de declarar
+    int a = 2;
+    int a, b,c;
+    */
+
+    int soma =  a + b;
+    //int subtracao = b - a; 
+   // int divisao = b/a;
+   // int multiplicacao = a*b;
+
+    printf("soma = %i",soma);
+
+
+    return 0;
+}
